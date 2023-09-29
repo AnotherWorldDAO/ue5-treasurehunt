cccee185cb261c24d75611ebb02460032c0018f2
Jackie Chia-Hsun Lee <jackylee0424@users.noreply.github.com>
2023-09-29 12:37:27 -0700

    add optimism-goerli testnet contracts

--- Solidity/README.md
+++ Solidity/README.md
@@ -7,6 +7,13 @@
 ## Deployment
 - `npx hardhat run --network goerli scripts\deploy_GamePrize.js`
 
+## Verification
+- `npx hardhat verify --network <network> <contract address>`
+
 ## Goerli Contracts
 - `GamePrize` - https://goerli.etherscan.io/address/0x30a43A490C1f9bFa04BB69f8Cb5A0989d742b405
 - `MockERC20` - https://goerli.etherscan.io/address/0x74BbDac19b8bE45B36560EA53A72C6cD0540cb17 
+
+## Optimism-Goerli Contracts
+- `GamePrize` - https://goerli-optimism.etherscan.io/address/0xc442372661eee647d01b0ef6cbc43768aaf141e6
+- `MockERC20` - https://goerli-optimism.etherscan.io/address/0x30a43a490c1f9bfa04bb69f8cb5a0989d742b405 
--- Solidity/hardhat.config.js
+++ Solidity/hardhat.config.js
@@ -24,22 +24,47 @@ module.exports = {
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [
-        //`0x${process.env.GOERLI_DEPLOYER}`,
-        //`0x${process.env.GOERLI_OPERATOR}`,
+        `0x${process.env.GOERLI_DEPLOYER}`, `0x${process.env.GOERLI_OPERATOR}`
       ],
     },
     optimism: {
       url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+    },
+    goerliop: {
+      url: `https://optimism-goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [
+        `0x${process.env.GOERLI_DEPLOYER}`, `0x${process.env.GOERLI_OPERATOR}`
+      ],
+      // gasPrice: 100, // may need to specify higher gas on OP
     },
     hardhat: {
       chainId: 1337,
     },
   },
   etherscan: {
+    customChains: [
+      {
+        network: "optimism",
+        chainId: 10,
+        urls: {
+          apiURL: "https://api-optimistic.etherscan.io/api",
+          browserURL: "https://optimistic.etherscan.io/",
+        },
+      },
+      {
+        network: "goerliop",
+        chainId: 420,
+        urls: {
+          apiURL: "https://api-goerli-optimistic.etherscan.io/api",
+          browserURL: "https://goerli-optimism.etherscan.io/",
+        },
+      },
+    ],
     apiKey: {
       mainnet: process.env.ETHERSCAN_API_KEY,
       goerli: process.env.ETHERSCAN_API_KEY,
       optimism: process.env.ETHERSCAN_OP_API_KEY,
+      goerliop: process.env.ETHERSCAN_OP_API_KEY,
     },
   },
 };
